986f7925690855b0541c014d0a485b5b693e796c
sarkazurkova <120708285+sarkazurkova@users.noreply.github.com>
2025-06-13 07:01:37 +0200

    Add files via upload

--- eshop/views.py
+++ eshop/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from models import Kniha, Zanr
 
 # Create your views here.
+# Pohled pro zobrazení domovské stránky
+def index(request):
+    zanr = 'povídky'
+    context = {
+        'nadpis': zanr,
+        'knihy': Kniha.objects.order_by('rok_vydani').filter(zanry__nazev=zanr),
+        'zanry': Zanr.objects.all()
+    }
+    return render(request, 'index.html', context=context)
