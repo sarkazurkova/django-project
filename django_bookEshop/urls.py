53ab82e37b6ce823416c1ab2bd355c3fa3427c72
sarkazurkova <120708285+sarkazurkova@users.noreply.github.com>
2025-06-20 08:48:18 +0200

    Add files via upload

--- django_bookEshop/urls.py
+++ django_bookEshop/urls.py
@@ -17,6 +17,8 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from django.views.generic import RedirectView
+from django.conf.urls.static import static
+from . import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -24,3 +26,6 @@ urlpatterns = [
     path('', RedirectView.as_view(url='eshop/')),
 ]
 
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
